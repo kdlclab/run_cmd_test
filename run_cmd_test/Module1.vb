ace44dbfe1b376c7fc29fe35063427e11be1da1e
= <=>
2018-10-04 00:08:49 -0700

    first commit

---
+++ run_cmd_test/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim procx As New Process()
+        With procx
+            'change the "/C to whatever command you like..."
+            .StartInfo = New ProcessStartInfo("cmd.exe", "/C type c:\the\file\source.txt > c:\the\file\destination.txt")
+            With .StartInfo
+                .CreateNoWindow = True
+                .UseShellExecute = False
+                .RedirectStandardOutput = True
+            End With
+            .Start()
+            .WaitForExit()
+        End With
+    End Sub
+
+End Module
